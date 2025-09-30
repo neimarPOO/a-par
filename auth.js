afcd576990adaa2e7c37bf1feaac6bf5af08d8ef
neimarPOO <neimarsmachado@gmail.com>
2025-09-30 15:43:32 -0300

    refactor(auth): Remove temporary force logout

--- auth.js
+++ auth.js
@@ -1,8 +1,6 @@
 const { createClient } = supabase;
 const _supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// Força o logout para limpar a sessão inválida
-_supabase.auth.signOut();
 
 const loginForm = document.getElementById('login-form');
 const loginBtn = document.getElementById('login-btn');
